337318a54fe020178ead33512a94c66412dc5e60
Ashwini Kaklij <ashwini.kaklij@avinetworks.com>
2017-10-07 16:07:36 +0530

    Fixed few variable reference issues

--- plot_graph.py
+++ plot_graph.py
@@ -11,56 +11,64 @@ class Earthquake(object):
         self.plot()
 
     def fetch_url(self):
+        """
+        Fetch earthquake data
+        :return:
+        """
         data = requests.get(self.request_url)
         response = data.json()
         return response
 
     def get_geo_data(self):
+        """
+        Format earthquake data
+        :return: list1, list2, list3
+        """
         data = self.fetch_url()
         lats = []
         lons = []
-        magnitudes = []
+        mags = []
         for res in data.values():
-            lons.append(res['lons'])
-            lats.append(res['lats'])
-            magnitudes.append(res['magnitudes'])
-        return lats, lons, magnitudes
+            lons.extend(res['lons'])
+            lats.extend(res['lats'])
+            mags.extend(res['magnitudes'])
+        return lats, lons, mags
 
     def plot(self):
-        """Data plotting"""
+        """
+        Data plotting
+        """
         map = Basemap(
             projection='moll', resolution='l',
             area_thresh=1000.0, lat_0=0, lon_0=-130
         )
         map.drawcoastlines(linewidth=0.25)
         map.drawcountries(linewidth=0.25)
-        map.fillcontinents(color='coral',lake_color='aqua')
+        map.fillcontinents(color='gray')
         map.drawmeridians(np.arange(0, 360, 30))
         map.drawparallels(np.arange(-90, 90, 30))
         min_marker_size = 4
-        lats, lons, statuses = self.get_geo_data()
-        for lon, lat, status in zip(lons, lats, statuses):
-            x, y = map(lon, lat)
-            if status is not None:
-                marker_string, size = self.set_marker_color(status)
-                msize = size * min_marker_size
-                map.plot(x, y, marker_string, markersize=msize)
+        lats, lons, mags = self.get_geo_data()
 
+        for lon, lat, mag in zip(lons, lats, mags):
+            x, y = map(lon, lat)
+            if mag is not None:
+                msize = mag * min_marker_size
+                marker_string = self.set_marker_color(mag)
+                map.plot(x, y, marker_string, markersize=msize)
         title_string = "Earthquakes of Magnitude 1.0 or Greater \n"
         title_string += str(self.params['start']) + ' through ' + str(self.params['end'])
         plt.title(title_string)
         plt.show()
 
-    def set_marker_color(self, magnitude):
-        if magnitude == "automatic":
-            # Green
-            return 'go', 2
-        elif magnitude == "reviewed":
-            # Yellow
-            return 'yo', 3
+    @staticmethod
+    def set_marker_color(magnitude):
+        if magnitude < 3.0:
+            return 'go'
+        elif magnitude < 5.0:
+            return 'yo'
         else:
-            # Red
-            return 'ro', 4
+            return 'ro'
 
 
 if __name__ == "__main__":
