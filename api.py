1e315b1deb44e969dbd6c6f72c9b08f166dc8abd
Ashwini Kaklij <ashwini.kaklij@avinetworks.com>
2017-10-07 15:28:15 +0530

    Classified code

--- api.py
+++ api.py
@@ -1,54 +1,59 @@
 import requests
 from multiprocessing.pool import ThreadPool
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
-
-# Constants
-URL = "https://earthquake.usgs.gov/fdsnws/event/1/query"
-LIMIT = 100
-TOTAL = 500
-PARAMS = {
-    'format': 'geojson',
-    'limit': LIMIT
-}
+api = Api(app)
 
 
-@app.route('/api/v1/earthquake-data', methods=['GET'])
-def index():
-    arguments = request.args
-    PARAMS.update({
-        'starttime': arguments.get('start'),
-        'endtime': arguments.get('end')
-    })
-    threads = int(round(TOTAL / LIMIT))
-    results = ThreadPool(threads).imap_unordered(fetch_url, range(1, TOTAL, LIMIT))
-    earth_quake_data = {}
-    lats = []
-    lons = []
-    mag  = []
-    for res, error in results:
-        if error is None:
-            for data in res['features']:
-                lons.append(data['geometry']['coordinates'][0])
-                lats.append(data['geometry']['coordinates'][1])
-                mag.append(data['properties']['mag'])
-        else:
-            return "error fetching %r: %s" % error
-    earth_quake_data.update({'properties': {'lats': lats, 'lons': lons, 'mag': mag}})
-    return jsonify(earth_quake_data)
+class EarthquakeData(Resource):
 
+    def __init__(self):
+        self.request_url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+        self.limit = 100
+        self.total = 500
+        self.param = {
+            'format': 'geojson',
+            'limit': self.limit
+        }
 
-def fetch_url(offset):
-    try:
-        PARAMS.update({'offset': offset})
-        res = requests.get(
-            URL, params=PARAMS
+    def get(self, start, end):
+        self.param.update({
+            'starttime': start,
+            'endtime': end
+        })
+        threads = int(round(self.total / self.limit))
+        results = ThreadPool(threads).imap_unordered(
+            self.fetch_url, range(1, self.total, self.limit)
         )
-        return res.json(), None
-    except Exception as e:
-        return None, e
+        earth_quake_data = {}
+        lats = []
+        lons = []
+        magnitudes = []
+        for res, error in results:
+            if error is None:
+                for data in res['features']:
+                    lons.append(data['geometry']['coordinates'][0])
+                    lats.append(data['geometry']['coordinates'][1])
+                    magnitudes.append(data['properties']['mag'])
+            else:
+                return "error fetching %r: %s" % error
+        earth_quake_data.update({'properties': {'lats': lats, 'lons': lons, 'magnitudes': magnitudes}})
+        return jsonify(earth_quake_data)
+
+    def fetch_url(self, offset):
+        try:
+            self.param.update({'offset': offset})
+            res = requests.get(
+                self.request_url, params=self.param
+            )
+            return res.json(), None
+        except Exception as e:
+            return None, e
+
+api.add_resource(EarthquakeData, '/api/v1/earthquake-data/<string:start>/<string:end>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
--- plot_graph.py
+++ plot_graph.py
@@ -3,57 +3,70 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
-URL = 'http://localhost:5002/api/v1/earthquake-data'
-PARAMS = {
-    'start': '2016-01-01',
-    'end': '2016-01-30'
-}
-response = requests.get(URL, params=PARAMS)
 
-data = response.json()
-lats = []
-lons = []
-magnitudes = []
-for res in data.values():
-    lons = res['lons']
-    lats = res['lats']
-    magnitudes = res['mag']
+class Earthquake(object):
+    def __init__(self, url, params={}):
+        self.request_url = url
+        self.params = params
+        self.plot()
 
-map = Basemap(
-    projection='moll', resolution='l',
-    area_thresh=1000.0, lat_0=0, lon_0=-130
-)
-map.drawcoastlines()
-map.drawcountries()
-map.drawlsmask()
-map.drawrivers()
-map.fillcontinents()
-map.bluemarble()
-map.drawmapboundary()
-map.fillcontinents(color='gray')
-map.drawmapboundary()
-map.drawmeridians(np.arange(0, 360, 30))
-map.drawparallels(np.arange(-90, 90, 30))
+    def fetch_url(self):
+        data = requests.get(self.request_url)
+        response = data.json()
+        return response
+
+    def get_geo_data(self):
+        data = self.fetch_url()
+        lats = []
+        lons = []
+        magnitudes = []
+        for res in data.values():
+            lons.append(res['lons'])
+            lats.append(res['lats'])
+            magnitudes.append(res['magnitudes'])
+        return lats, lons, magnitudes
+
+    def plot(self):
+        """Data plotting"""
+        map = Basemap(
+            projection='moll', resolution='l',
+            area_thresh=1000.0, lat_0=0, lon_0=-130
+        )
+        map.drawcoastlines(linewidth=0.25)
+        map.drawcountries(linewidth=0.25)
+        map.fillcontinents(color='coral',lake_color='aqua')
+        map.drawmeridians(np.arange(0, 360, 30))
+        map.drawparallels(np.arange(-90, 90, 30))
+        min_marker_size = 4
+        lats, lons, statuses = self.get_geo_data()
+        for lon, lat, status in zip(lons, lats, statuses):
+            x, y = map(lon, lat)
+            if status is not None:
+                marker_string, size = self.set_marker_color(status)
+                msize = size * min_marker_size
+                map.plot(x, y, marker_string, markersize=msize)
+
+        title_string = "Earthquakes of Magnitude 1.0 or Greater \n"
+        title_string += str(self.params['start']) + ' through ' + str(self.params['end'])
+        plt.title(title_string)
+        plt.show()
+
+    def set_marker_color(self, magnitude):
+        if magnitude == "automatic":
+            # Green
+            return 'go', 2
+        elif magnitude == "reviewed":
+            # Yellow
+            return 'yo', 3
+        else:
+            # Red
+            return 'ro', 4
 
 
-def set_marker_color(magnitude):
-    if magnitude < 3.0:
-        return 'go'
-    elif magnitude < 5.0:
-        return 'yo'
-    else:
-        return 'ro'
-
-
-min_marker_size = 2.5
-for lon, lat, mag in zip(lons, lats, magnitudes):
-    x, y = map(lon, lat)
-    if mag is not None:
-        msize = mag * min_marker_size
-        marker_string = set_marker_color(mag)
-        map.plot(x, y, marker_string, markersize=msize)
-
-title_string = "Earthquakes of Magnitude 1.0 or Greater \n"
-title_string += str(PARAMS['start']) + ' through ' + str(PARAMS['end'])
-plt.title(title_string)
-plt.show()
+if __name__ == "__main__":
+    PARAMS = {
+        'start': '2016-01-01',
+        'end': '2016-01-30'
+    }
+    URL = 'http://localhost:5002/api/v1/earthquake-data/' + PARAMS['start'] + '/' + PARAMS['end']
+    earthquake = Earthquake(URL, PARAMS)
